d190ac1c95a986fda2dc905edb48aaa84e7e05c7
hp-netbook <maksim.levental@gmail.com>
2016-03-28 12:35:02 -0400

    forgot depth first traversal again :( i

--- ch16/huffman.py
+++ ch16/huffman.py
@@ -1,9 +1,71 @@
 from ch10.treenode import TreeNode
-from queue import PriorityQueue
+from queue import PriorityQueue, Queue
+
+
+class HuffNode(TreeNode):
+    def __init__(self,freq=0,**kwargs):
+        super().__init__(**kwargs)
+        self.freq = freq
 
 class HuffTree():
     def __init__(self):
         self.root = TreeNode()
 
-    def encode(self,ls):
-        q = PriorityQueue(ls)
+    def encode(self,chars):
+        q = PriorityQueue()
+        for freq,char in chars:
+            q._put((freq,HuffNode(freq=freq,val=char)))
+
+        for _ in range(len(chars)-1):
+            left = q._get()
+            right = q._get()
+            freq = left[1].freq + right[1].freq
+            z = HuffNode(freq=freq,lchild=left[1],rchild=right[1])
+            q._put((freq,z))
+
+        self.root = q._get()[1]
+
+        # push the last good one
+        stk = [self.root]
+        code = []
+        codes = []
+        while len(stk) > 0:
+            ptr = stk[-1]
+            if ptr.lchild is not None:
+                code.append(0)
+                stk.append(ptr.lchild)
+            elif ptr.rchild is not None:
+                code.append(1)
+                stk.append(ptr.rchild)
+            else:
+
+
+
+
+            if ptr is not None:
+                stk.append(ptr)
+                ptr = ptr.lchild
+                if ptr is not None: code.append(0)
+            else:
+                p = stk.pop()
+                if type(p.val) is str:
+                    code.append(p.val)
+                    codes.append(code)
+                    code = code[:]
+                code.pop()
+
+                ptr = p.rchild
+                if ptr is not None: code.append(1)
+
+        return codes
+
+
+    def print(self):
+        st = map(lambda x: ''.join(x),self.root.print())
+        print(*st,sep='\n')
+
+if __name__ == '__main__':
+    chars = [(45, 'a'), (13, 'b'), (12, 'c'), (16, 'd'), (9, 'e'), (5, 'f')]
+    h = HuffTree()
+    print(h.encode(chars))
+    h.print()
