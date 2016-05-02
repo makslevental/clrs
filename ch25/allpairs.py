e7b2cf6972e02f4b482a6d8c8bec216ec49fa22f
maksim <maksim.levental@gmail.com>
2016-05-01 23:05:38 -0400

    floyd warshall and transitive closure

--- ch25/allpairs.py
+++ ch25/allpairs.py
@@ -3,6 +3,9 @@ import numpy as np
 def extend(L,W):
     Lp = np.full_like(L, np.inf)
     for i in range(L.shape[0]):
+        L[i,i] = 0
+
+    for i in range(L.shape[0]):
         for j in range(L.shape[1]):
             Lp[i,j] = min(L[i,:]+W[:,j])
 
@@ -24,6 +27,53 @@ def allpairs(W):
         else: y,Wp,n = extend(Wp,y), extend(Wp,Wp), (n-1)/2
     return extend(Wp,y)
 
+
+def floydwarshall(W):
+    n = W.shape[0]
+
+    Dk_1 = W
+
+    for k in range(n):
+        Dk = np.full_like(W,np.inf)
+        for i in range(n):
+            for j in range(n):
+                # either k is not an intermediate vertex of the shortest path
+                # from i to j or k is and then the shortest path
+                # from i to j is the path from i to k and then k to j
+                Dk[i,j] = min(Dk_1[i,j],Dk_1[i,k]+Dk_1[k,j])
+        Dk_1 = Dk
+
+    return Dk
+
+def trans_closure(W):
+    n = W.shape[0]
+
+    Tk_1 = W.full_like(W,False)
+    for i in range(n):
+        for j in range(n):
+            if (not W[i,j] is np.inf and W[i,j] > 0) or i == j:
+                Tk_1[i,j] = True
+
+    for k in range(n):
+        Tk = np.full_like(W,False)
+        for i in range(n):
+            for j in range(n):
+                Tk = Tk_1[i,j] or (Tk_1[i,k] and Tk_1[k,j])
+        Tk_1 = Tk
+
+    return Tk
+
+def findpi(L,W):
+    n = L.shape[0]
+    pi = np.full_like(L,None)
+    for i in range(n):
+        for j in range(n):
+            # guess and check
+            for k in range(n):
+                if L[i,k] + W[k,j] == L[i,j]:
+                    pi[i,j] = k
+
+
 if __name__ == '__main__':
     A = np.ones((10,10))
     B = np.full_like(A,5)
@@ -33,7 +83,8 @@ if __name__ == '__main__':
 
 # 25.1-5
 
-# 25.1-6
+# 25.1-6 dumbest shit: loop over everything and check against edge weights to see shortest path
+# between two vertices is edge weight plus shortest path to first vertex
 
 # 25.1-7
 
